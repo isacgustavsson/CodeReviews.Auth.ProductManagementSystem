cc3aa9afed9a9ffb2ee622a875b3415c9f43079a
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2026-03-08 18:43:45 +0100

    Sees database with default user roles for authentication

--- UI/Data/ApplicationDbContext.cs
+++ UI/Data/ApplicationDbContext.cs
@@ -1,8 +1,29 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace UI.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
 {
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<IdentityRole>().HasData(
+            new IdentityRole
+            {
+                Id = "1",
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            },
+            new IdentityRole
+            {
+                Id = "2",
+                Name = "Staff",
+                NormalizedName = "STAFF"
+            }
+        );
+    }
+
 }

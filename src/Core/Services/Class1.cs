83484894208069c61e3b45eb5085702ebf7a6003
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2026-03-09 12:40:28 +0100

    Add ProductService to use IproductRepository to encapsulate product business logic

--- src/Core/Services/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Core.Services;
-
-public class Class1
-{
-
-}
---
+++ src/Core/Services/ProductService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+
+namespace Core.Services;
+
+public class ProductService(IProductRepository productRepository)
+{
+    private readonly IProductRepository _productRepository = productRepository;
+
+    public async Task<List<Product>> GetAllProductsAsync() => await _productRepository.GetAllProductsAsync();
+
+    // public void AddProduct()
+    // {
+    //     _productRepository.AddProduct();
+    // }
+}

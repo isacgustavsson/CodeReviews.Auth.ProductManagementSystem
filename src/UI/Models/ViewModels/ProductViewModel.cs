d5592f05a115cdae72bb73d510b9cdd33b937f1c
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2026-03-10 10:30:37 +0100

    Refactor: Modify ProductViewModel and View "Product" to use field "Id"

--- src/UI/Models/ViewModels/ProductViewModel.cs
+++ src/UI/Models/ViewModels/ProductViewModel.cs
@@ -2,6 +2,7 @@ namespace UI.Models.ViewModels;
 
 public class ProductViewModel
 {
+    public int Id { get; }
     public required string? Name { get; set; }
     public decimal Price { get; set; }
 }
